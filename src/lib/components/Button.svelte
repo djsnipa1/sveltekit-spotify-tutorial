c57632e79aa507c3d0dccf04839bdf5e495a3001
Chad Boyce <chadboyce@gmail.com>
2023-11-26 09:49:14 +0000

    end of lesson 13

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-import { HTMLAnchorAttributes, HTMLButtonAttributes } from "svelte/elements"
+	import type { HTMLAnchorAttributes, HTMLButtonAttributes } from 'svelte/elements';
 
-type Element = $$Generic<'button' | 'a'>;
+	type Element = $$Generic<'button' | 'a'>;
 
-interface ButtonComponentElements {
-	button: HTMLButtonAttributes;
-	a: HTMLAnchorAttributes;
-}
+	interface ButtonComponentElements {
+		button: HTMLButtonAttributes;
+		a: HTMLAnchorAttributes;
+	}
 
-type $$Props = ButtonComponentElements[Element] & {
-element: Element;	
-variant?: 'solid' | 'outline' | 'danger';
-className?: string;	
-};
+	type $$Props = ButtonComponentElements[Element] & {
+		element: Element;
+		variant?: 'solid' | 'outline' | 'danger';
+		className?: string;
+	};
 	export let element: Element;
 	export let variant: 'solid' | 'outline' | 'danger' = 'solid';
 	export let className: string = '';
