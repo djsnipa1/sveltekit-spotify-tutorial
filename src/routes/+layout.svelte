57e05e0eebe2bd36e33602b066c75ba8ba416cfe
Chad Boyce <chadboyce@gmail.com>
2023-12-07 19:53:10 +0000

    end lesson 16 but one prob needs fixed

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,10 +4,22 @@
 	import '../styles/main.scss';
 	import type { LayoutData } from './$types';
 
+
+	let topbar: HTMLElement;
+	let scrollY: number;	
+	let headerOpacity = 0;
+	
+	$: if(topbar) {
+		console.log(topbar && scrollY / topbar.offsetHeight)
+		headerOpacity = scrollY / topbar.offsetHeight < 1 ? scrollY / topbar.offserHeight : 1;
+	} 
+	
 	export let data: LayoutData;
 
 	$: user = data.user;
 </script>
+
+<svelte:window bind:scrollY />
 
 <div id="main">
 	{#if user}
@@ -16,7 +28,11 @@
 		</div>
 	{/if}
 	<div id="content">
-		<main id="main-content">
+		<div id="topbar" bind:this={topbar}>
+			<div class="topbar-bg" style:background-color="var(--header-color)" style:opacity={headerOpacity} />
+			topbar
+		</div>
+		<main id="main-content" class:logged-in={user}>
 			<slot />
 		</main>
 	</div>
@@ -27,10 +43,34 @@
 		display: flex;
 		#content {
 			flex: 1;
+			#topbar {
+				position: fixed;
+				height: var(--header-height);
+				padding: 0 15px;
+				display: flex;
+				align-items: center;
+				width: 100%;
+				z-index: 100;
+				.topbar-bg {
+					position: absolute;
+					width: 100%;
+					height: 100%;
+					top: 0;
+					left: 0;
+					z-index: -1;
+				}
+				@include breakpoint.up('md') {
+					padding: 0 30px;
+					width: calc(100% - var(--sidebar-width));
+				}
+			}
 			main#main-content {
 				padding: 30px 15px 60px;
 				@include breakpoint.up('md') {
 					padding: 30px 30px 60px;
+				}
+				&.logged-in {
+				padding-top: calc(30px + var(--header-height));
 				}
 			}
 		}
